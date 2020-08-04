0404b5f0ef3c95f3f655fff5983593fb365ef52e
kp1129 <katya.p.1129@gmail.com>
2020-08-03 18:44:46 -0700

    solved coding challenge

--- scratchpad.py
+++ scratchpad.py
@@ -46,20 +46,23 @@
 # 
 # another attempt
 
-# def find_max_profit(prices):
-#     max_profit_val, current_max_val = 0, 0 
-#     for price in reversed(prices):                       
-#         current_max_val = max(current_max_val, price)          
-#         potential_profit = current_max_val - price          
-#         max_profit_val = max(potential_profit, max_profit_val)
+def find_max_profit(prices):
+    max_profit_val, current_max_val = 0, 0 
+    for price in reversed(prices):                       
+        current_max_val = max(current_max_val, price)          
+        potential_profit = current_max_val - price          
+        max_profit_val = max(potential_profit, max_profit_val)
 
-#     if max_profit_val == 0:
-#         for price in reversed(prices):
-#             current_max_val = min(current_max_val, price)          
-#             potential_profit = current_max_val - price          
-#             max_profit_val = min(potential_profit, max_profit_val)
+    if max_profit_val > 0:
+        return max_profit_val
+    else:    
+        biggest_loss = 0 - current_max_val
+        for price in reversed(prices):
+            current_max_val = max(current_max_val, price)          
+            potential_profit = current_max_val - price         
+            max_profit_val = max(potential_profit, max_profit_val)
 
-#     return max_profit_val
+        return biggest_loss + max_profit_val
 
 
 # third attempt
